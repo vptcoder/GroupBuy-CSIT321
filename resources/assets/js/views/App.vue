95c8e3f9a0067ad586bbd4298240199ef3ec4a92
vpt <lparesse@outlook.com>
2021-03-15 16:01:52 +0800

    Update to the app name and nav bar

--- resources/assets/js/views/App.vue
+++ resources/assets/js/views/App.vue
@@ -1,25 +1,11 @@
 <template>
+<!-- NOTES: This file is for navigation bar code. -->
 	<div>
 		<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-			<div class="container">
-				<router-link :to="{name: 'home'}" class="navbar-brand">Big Store</router-link>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<!-- Left Side Of Navbar -->
-					<ul class="navbar-nav mr-auto"></ul>
-					<!-- Right Side Of Navbar -->
-					<ul class="navbar-nav ml-auto">
-						<router-link :to="{ name: 'login' }" class="nav-link" v-if="!isLoggedIn">Login</router-link>
-						<router-link :to="{ name: 'register' }" class="nav-link" v-if="!isLoggedIn">Register</router-link>
-						<span v-if="isLoggedIn">
-							<router-link :to="{ name: 'userboard' }" class="nav-link" v-if="user_type == 0"> Hi, {{name}}</router-link>
-							<router-link :to="{ name: 'admin' }" class="nav-link" v-if="user_type == 1"> Hi, {{name}}</router-link>
-						</span>
-						<li class="nav-link" v-if="isLoggedIn" @click="logout"> Logout</li>
-					</ul>
-				</div>
+			<div class="newcontainer">
+				<router-link :to="{name: 'home'}" class="navbar-brand">G-BUY</router-link>
+				<input type="text" class="form-control navbar-search" name="Search" placeholder="Search">
+
 			</div>
 		</nav>
 		<main class="py-4">
--- resources/assets/js/views/Home.vue
+++ resources/assets/js/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
+<!-- NOTES: This file is for home page -->
     <div>
         <div class="container-fluid hero-section d-flex align-content-center justify-content-center flex-wrap ml-auto">
-            <h2 class="title">Welcome to the bigStore</h2>
+            <h2 class="title">Welcome to G-BUY</h2>
         </div>
         <div class="container">
             <div class="row">
