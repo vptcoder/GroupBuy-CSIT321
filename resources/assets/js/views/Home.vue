0e76315eb7300d4ca610f923cb0677a377bcfe1a
xinroul <limxinrou@hotmail.com>
2021-03-23 22:47:10 +0800

    Home&Single
    Home and Singleproduct page. Not exactly done yet buy layout.

--- resources/assets/js/views/Home.vue
+++ resources/assets/js/views/Home.vue
@@ -1,28 +1,52 @@
 <template>
-<!-- NOTES: This file is for home page -->
     <div>
-        <div class="container-fluid hero-section d-flex align-content-center justify-content-center flex-wrap ml-auto">
-            <h2 class="title">Welcome to as G-BUY</h2>
-        </div>
+
         <div class="container">
             <div class="row">
-                <div class="col-md-12">
-                    <div class="row">
-                        <div class="col-md-4 product-box" v-for="(product,index) in products" :key="index">
-                            <router-link :to="{ path: '/products/'+product.id}">
-                                <img :src="product.image" :alt="product.name">
-                                <h5><span v-html="product.name"></span>
-                                    <span class="small-text text-muted float-right">$ {{product.price}}</span>
-                                </h5>
-                                <button class="col-md-4 btn btn-sm btn-primary float-right">Buy Now</button>
-                            </router-link>
-                        </div>
-                    </div>
-                </div>
+                 <div class="col-sm-3" ><img src="/assets/img/icons/shoes.png"><b>Food</b></div>
+                 <div class="col-sm-3" ><img src="/assets/img/icons/shoes.png"><b>Shoes</b></div>
+                 <div class="col-sm-3" ><img src="/assets/img/icons/shoes.png"><b>Garden</b></div>
+                 <div class="col-sm-3" ><img src="/assets/img/icons/shoes.png"><b>Boy</b></div>
             </div>
+            <div class="row">
+                 <div class="col-sm-3" ><img src="/assets/img/icons/shoes.png"><b>pig</b></div>
+                 <div class="col-sm-3" ><img src="/assets/img/icons/shoes.png"><b>Pets</b></div>
+                 <div class="col-sm-3" ><img src="/assets/img/icons/shoes.png"><b>Fashion</b></div>
+                 <div class="col-sm-3" ><img src="/assets/img/icons/shoes.png"><b>GG</b></div>
+            </div>
+        </div>
+        <div style="text-align:center">
+            <button type="button" style="background-color:#FADBD8;width:20px;height:10px"></button>
+            <button type="button" style="width:20px;height:10px"></button>
+        </div>
+        <br>
+        <div class="container">
+            <row>
+                <b>Popular Groupbuy</b>
+                <input type="button" value="" style="margin-left:120px;width: 10px; height: 10px; background-color:#CCD1D1;border-radius:50%;border: none">
+                <input type="button" value="" style="margin-left:10px;width: 10px; height: 10px; border-radius:50%;border: none">
+                <input type="button" value="" style="margin-left:10px;width: 10px; height: 10px; border-radius:50%;border: none">
+            </row>
+            <div class="row" >
+                 <div class="col-sm-4"><img src="/assets/img/products/nike.png"> </div>
+                 <div class="col-sm-4"><img src="/assets/img/products/nike.png"></div>
+                 <div class="col-sm-4"><img src="/assets/img/products/nike.png"></div>
+            </div>
+        </div>
+        <br><br>
+         <div class ="item prod" v-for="(product,index) in products" :key="index">
+                  <router-link :to="{ path: '/products/'+product.id}">
+                    <img :src="product.image" :alt="product.name">
+                    <span class ="name" v-html="product.name"></span><br>
+                    <div align="center"><span class="price">$ {{product.price}}</span></div>
+                    <button class="col-md-4 btn btn-sm btn-primary float-right">Buy Now</button>
+                  </router-link>
+               
         </div>
     </div>
 </template>
+
+<div></div>
 
 <script>
     export default {
@@ -38,22 +62,64 @@
 </script>
 
 <style scoped>
-.small-text {
-    font-size: 14px;
+
+.col-sm-3{
+  float: center;
+  display: block;
+  background-color: #f4f4f4;
+  width: 22%;
+  height: 15%;
+  margin-left: 9px;
+  margin-top: 5px;
+  text-align:center;
 }
-.product-box {
-    border: 1px solid #cccccc;
-    padding: 10px 15px;
+.col-sm-3 img {
+  min-height: 60%;
+  max-height: 60%;
+  min-width: 60%;
+  max-width: 60%;
+  text-align : center;
 }
-.hero-section {
-    height: 30vh;
-    background: #ababab;
-    align-items: center;
-    margin-bottom: 20px;
-    margin-top: -20px;
+.col-sm-4 {
+  margin-left:5%;
+  width: 26%;
+  height: 15%;
+  background-color: #f4f4f4;
 }
-.title {
-    font-size: 60px;
-    color: #ffffff;
+
+.col-sm-4 img {
+  min-height: 70%;
+  max-height: 70%;
+  min-width: 100%;
+  max-width: 100%;
 }
+
+.item.prod {
+  float: left;
+  display: block;
+  background-color: #FBEEE6;
+  width: 45%;
+  height: 15%;
+  margin-left:13px;
+  margin-top:10px;
+}
+
+.item.prod img{
+  min-height: 70%;
+  max-height: 70%;
+  min-width: 100%;
+  max-width: 100%;
+} 
+.item.prod .name {
+  font-size: 12px;
+  color:red;
+}
+
+.item.prod .price {
+  font-size: 14px;
+  font-weight: bold;
+  color:black;
+}
+
+
 </style>
---
+++ resources/assets/js/views/Home_2.vue
@@ -0,0 +1,205 @@
+<template>
+
+<!-- NOTES: This file is for home page -->
+    <div>
+         
+         <!--Segment One -- Category -->
+              <ul id="category-selection" class="segment">
+                <li class="item cat"><img src="/assets/img/icons/shoes.png"><i class="content">asd</i></li>
+                <li class="item cat"><img src="/assets/img/icons/food.png"><i class="content">asd</i></li>
+                <li class="item cat"><img src="/assets/img/icons/furniture.png"><i class="content">asd</i></li>
+                <li class="item cat"><img src="/assets/img/icons/bags.png"><i class="content">asd</i></li>
+                <li class="item cat"><img src="/assets/img/icons/bath.png"><i class="content">asd</i></li>
+              </ul>
+              <br>
+              <br>
+              <br>
+        <!--Segment Two -- Popular -->
+              <ul id="pop-selection" class="segment"  >
+                <li class="item pop"><img src="/assets/img/products/nike.png">
+                    <div class="content">
+                      <div class="price">Asd </div>
+                      <div class="price">123 </div>
+                    </div>
+                </li>
+                <li class="item pop"><img src="/assets/img/products/fila.png">
+                    <div class="content">
+                      <div class="name">baba </div>
+                      <div class="price">331 </div>
+                      <div class="price">321 </div>
+                    </div>
+                </li>    
+                <li class="item pop"><img src="/assets/img/products/fila.png">
+                    <div class="content">
+                      <div class="name">baba </div>
+                      <div class="price">331 </div>
+                      <div class="price">321 </div>
+                    </div>
+                </li>                           
+              </ul>
+              <br>
+              <br>
+              <br>
+               <!--Segment Three -- Products -->
+              <ul id="pop-selection" class="segment">
+              <div class ="item prod" v-for="(product,index) in products" :key="index">
+                  <router-link :to="{ path: '/products/'+product.id}">
+                    <img :src="product.image" :alt="product.name">
+                    <span class ="name" v-html="product.name"></span>
+                    <span class="price">$ {{product.price}}</span>
+                    <button class="col-md-4 btn btn-sm btn-primary float-right">Buy Now</button>
+                  </router-link>
+              </div>
+              </ul>
+              <br>
+              <br>
+              <br>
+ 
+ 
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                products : []
+            }
+        },
+        mounted(){
+            axios.get("api/products/").then(response => this.products = response.data)      
+        }
+    }
+    //Vue.loadScript("C:/xampp/htdocs/GroupBuy-CSIT321/resources/assets/js/player-export.js")
+
+</script>
+
+<style>
+ 
+.small-text {
+    font-size: 14px;
+}
+.product-box {
+    border: 1px solid #cccccc;
+    padding: 10px 15px;
+}
+.hero-section {
+    height: 30vh;
+    /*background: #ababab;*/
+    align-items: center;
+    margin-bottom: 20px;
+    margin-top: -20px;
+}
+.title {
+    font-size: 60px;
+    color: #ffffff;
+}
+.segment
+{
+       *zoom: 1;
+}
+.segment:after{
+  content: "\0020";
+  display: block;
+  height: 0;
+  clear: both;
+  }
+
+
+/* Item Content Inside Segment One _ Main*/
+.item {
+  float: left;
+  display: block;
+  background-color: #f4f4f4;
+  width: 22%;
+  height: 15%;
+  margin: 2% 1%;
+ 
+
+}
+ 
+ 
+.item img {
+  min-height: 100%;
+  max-height: 100%;
+  min-width: 100%;
+  max-width: 100%;
+  text-align : center;
+
+}
+
+.item.cat img {
+  min-height: 100%;
+  max-height: 100%;
+  min-width: 50%;
+  max-width: 50%;
+  padding: 1% 30%;
+  text-align : center;
+}
+.item .content {
+  align-content: center;
+  color:black;
+  left: 50%;
+  margin: 2% 30%;
+  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+ 
+}
+
+/* Segment Two */
+.item.pop {
+  margin: 2% 3%;
+  width: 26%;
+  height: 15%;
+}
+
+.item.pop img {
+  min-height: 70%;
+  max-height: 70%;
+  min-width: 100%;
+  max-width: 100%;
+}
+
+.item .pop .content{
+  font-size: 14px;
+ 
+}
+
+/* Segment Three */
+.item.prod {
+  margin: 2% 4%;
+  width: 40%;
+  height: 20%;
+}
+
+.item.prod img{
+  min-height: 70%;
+  max-height: 70%;
+  min-width: 100%;
+  max-width: 100%;
+} 
+.item.prod .name {
+  font-size: 14px;
+  color:red;
+
+ 
+}
+
+.item.prod .price {
+  font-size: 14px;
+
+}
+.segment 
+/** Outside **/{
+  background-color: white;
+}
+  
+ 
+ 
+
+body {
+  background-color: #f7f7f7;
+  font: 14px / 1.2 "Oxygen", Helvetica, Arial;
+}
+
+ 
+ 
+</style>
