e97ab24a359f6af57a248118cd7fe92c03736375
Van Phuong Thanh <vptcoder@users.noreply.github.com>
2021-03-23 23:09:51 +0800

    Update SingleProduct.vue

--- resources/assets/js/views/SingleProduct.vue
+++ resources/assets/js/views/SingleProduct.vue
@@ -1,22 +1,51 @@
 <template>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8 offset-md-2">
-				<img :src="product.image" :alt="product.name">
-				<h3 class="title" v-html="product.name"></h3>
-				<p class="text-muted">{{product.description}}</p>
-				<h4>
-					<span class="small-text text-muted float-left">$ {{product.price}}</span>
-					<span class="small-text float-right">Available Quantity: {{product.units}}</span>
-				</h4>
-				<br>
-				<hr>
-				<router-link :to="{ path: '/checkout?pid='+product.id }" class="col-md-4 btn btn-sm btn-primary float-right">Buy Now</router-link>
-			</div>
+    
+	<div>
+		 <div class="sale"> <img src="/assets/img/icons/badge-sale.svg.png"></div>
+		 <div class="img" ><img :src="product.image" :alt="product.name">
+		 </div>
+		 <div>
+			  <row>
+                <input type="button" value="" style="margin-left:155px;width: 10px; height: 10px; background-color:#CCD1D1;border-radius:50%;border: none">
+                <input type="button" value="" style="margin-left:10px;width: 10px; height: 10px; border-radius:50%;border: none">
+                <input type="button" value="" style="margin-left:10px;width: 10px; height: 10px; border-radius:50%;border: none">
+		      </row>
+		  </div>	
+		  <span class ="name" v-html="product.name"></span><br>  
+		  <hr>
+		  <div style="margin-left:30px">
+			  <row>
+			  Color: <b>MEDIUM BROWN NUMBUCK</b>
+			  <br>
+			  Style: <b>1012345</b> 
+			  <br>
+			  <b style="color:#A93226;font-size:25px;margin-left:10px;">${{product.price}}</b>
+			  <img src="/assets/img/icons/Oval 9.png" style="margin-left:150px">
+			  <b style="margin-left:10px">1</b>
+			  <img src="/assets/img/icons/Oval 8.png" style="margin-left:10px">
+			  </row><br>
+			  <row>
+			  <b style="color:#A93226;font-size:15px;">SGD</b><b style="color:black;font-size:15px;margin-left:5px;text-decoration:line-through;">$ 280</b>
+			  <lable style="margin-left:160px;color:red;">9 slots left</lable>
+			  </row>
+		  </div>
+		  <hr>
+		  <div>
+			  <img src="/assets/img/icons/bb.png"  style="margin-left:30px;"> <b>15 days return</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			  <img src="/assets/img/icons/gg.png"> <b>100% Authentic</b>
+			  <br>
+			  <img src="/assets/img/icons/car.png"  style="margin-left:30px;"><b> Free Shipping</b>
+		  </div>
+		  <hr>
+        <div>
+			<b style="Front-size:20px;margin-left:30px;">Description:</b>
+			<br>
+            <p style="margin-left:30px;">{{product.description}}</p>			
 		</div>
-	</div>
+		<router-link :to="{ path: '/checkout?pid='+product.id }" class="col-md-4 btn btn-sm btn-primary float-right">Buy Now</router-link>
+	</div>	
 </template>
-  
+
 <script>
 export default {
 	data(){
@@ -32,6 +61,31 @@ export default {
 </script>
 
 <style scoped>
-.small-text { font-size: 18px; }
-.title { font-size: 36px; }
+.sale{
+            height: 30px;
+            width: 30px;
+            position: relative;
+            left: 300px;
+            top:20px;
+}
+
+
+.img{
+	background-color:#f4f4f4;
+	width:300px;
+	height:170px;
+	margin-left:40px;
+}
+.img img{
+  min-height: 100%;
+  max-height: 100%;
+  min-width: 100%;
+  max-width: 100%;
+
+} 
+.name {
+  font-size: 12px;
+  color:black;
+  margin-left:70px;
+}
 </style>
