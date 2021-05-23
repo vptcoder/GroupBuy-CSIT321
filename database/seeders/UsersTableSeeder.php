844cf0c3bfc380decbc67bdf639524b7d8de1def
Van Phuong Thanh <vptcoder@users.noreply.github.com>
2021-05-23 21:06:35 +0800

    bugfix popular products without token

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -32,8 +32,8 @@ class UsersTableSeeder extends Seeder
         
         $testers = [
             [ 
-                'username' => 'tester1'
-                , 'name' => 'tester1'
+                'username' => 'Lim Xin Rou'
+                , 'name' => 'limxr'
                 , 'email' => 'tester1_vptcoder@gbuytest.com'
                 , 'shipping_streetaddress' => '01 Test Street, #11-01'
                 , 'shipping_city' => 'Singapore'
@@ -43,8 +43,8 @@ class UsersTableSeeder extends Seeder
                 , 'created_at' => Carbon::now(),
             ]
             , [
-                'username' => 'tester2'
-                , 'name' => 'tester2'
+                'username' => 'Ernest Goh'
+                , 'name' => 'earnestalphaboi'
                 , 'email' => 'tester2_vptcoder@gbuytest.com'
                 , 'shipping_streetaddress' => '02 Test Street, #11-02'
                 , 'shipping_city' => 'Singapore'
@@ -54,12 +54,67 @@ class UsersTableSeeder extends Seeder
                 , 'created_at' => Carbon::now(),
             ]
             , [
-                'username' => 'tester3'
-                , 'name' => 'tester3'
+                'username' => 'Yang Sean'
+                , 'name' => 'yangss'
                 , 'email' => 'tester3_vptcoder@gbuytest.com'
                 , 'shipping_streetaddress' => '03 Test Street, #11-03'
                 , 'shipping_city' => 'Singapore'
                 , 'shipping_postalcode' => '666663'
+                , 'password' => bcrypt('aacc12345')
+                , 'is_admin' => false 
+                , 'created_at' => Carbon::now(),
+            ]
+            , [
+                'username' => 'Goh Lou Yang'
+                , 'name' => 'gohly'
+                , 'email' => 'tester4_vptcoder@gbuytest.com'
+                , 'shipping_streetaddress' => '03 Simin Street, #7-13'
+                , 'shipping_city' => 'Singapore'
+                , 'shipping_postalcode' => '667113'
+                , 'password' => bcrypt('aacc12345')
+                , 'is_admin' => false 
+                , 'created_at' => Carbon::now(),
+            ]
+            , [
+                'username' => 'Ang Ding Wan'
+                , 'name' => 'angdw'
+                , 'email' => 'tester5_vptcoder@gbuytest.com'
+                , 'shipping_streetaddress' => '04 Simin Street, #5-10'
+                , 'shipping_city' => 'Singapore'
+                , 'shipping_postalcode' => '664510'
+                , 'password' => bcrypt('aacc12345')
+                , 'is_admin' => false 
+                , 'created_at' => Carbon::now(),
+            ]
+            , [
+                'username' => 'Cheok Jia Xin'
+                , 'name' => 'cheokjx'
+                , 'email' => 'tester6_vptcoder@gbuytest.com'
+                , 'shipping_streetaddress' => '10 How Sun Drive, #2-28'
+                , 'shipping_city' => 'Singapore'
+                , 'shipping_postalcode' => '510520'
+                , 'password' => bcrypt('aacc12345')
+                , 'is_admin' => false 
+                , 'created_at' => Carbon::now(),
+            ]
+            , [
+                'username' => 'Albert Brand'
+                , 'name' => 'alboy22'
+                , 'email' => 'tester7_vptcoder@gbuytest.com'
+                , 'shipping_streetaddress' => '11 Keong Saik Drive,'
+                , 'shipping_city' => 'Singapore'
+                , 'shipping_postalcode' => '544511'
+                , 'password' => bcrypt('aacc12345')
+                , 'is_admin' => false 
+                , 'created_at' => Carbon::now(),
+            ]
+            , [
+                'username' => 'Emily Whitehead'
+                , 'name' => 'whitequeen'
+                , 'email' => 'tester8_vptcoder@gbuytest.com'
+                , 'shipping_streetaddress' => '8 Tampines Street, #20-05'
+                , 'shipping_city' => 'Singapore'
+                , 'shipping_postalcode' => '628225'
                 , 'password' => bcrypt('aacc12345')
                 , 'is_admin' => false 
                 , 'created_at' => Carbon::now(),
